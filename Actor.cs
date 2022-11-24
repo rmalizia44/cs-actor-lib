5c5e52028bee4433e99a9781f566cc6eb827545f
rmalizia44 <rmalizia44@gmail.com>
2022-11-24 13:08:56 -0300

    Refatora delay de mensagens e timer do actor

--- Actor.cs
+++ Actor.cs
@@ -2,17 +2,57 @@ using System.Threading.Channels;
 
 namespace Actors;
 
+internal class EventScheduled: Event {
+    public readonly Actor Actor;
+    public EventScheduled(Actor actor, object data, long timestamp): base(data, timestamp) {
+        Actor = actor;
+    }
+}
+
 public class Actor {
-    private static readonly Exception Exception = new NotImplementedException();
-    private readonly DateTime StartTime;
-    private readonly Timer Timer;
+    private static readonly DateTime StartTime = DateTime.Now;
+    private static readonly Timer Timer = new(o => NotifyScheduled());
+    private static readonly List<EventScheduled> UnsafeMsgScheduled = new();
+    private static long CalcTimestamp() {
+        return (long)((DateTime.Now - StartTime).TotalMilliseconds + 0.5);
+    }
+    private static void UpdateTimer(long timeout) {
+        if(timeout < 0) {
+            timeout = 0;
+        }
+        Timer.Change(timeout, Timeout.Infinite);
+    }
+    private static bool PushScheduled(EventScheduled msg) {
+        lock(UnsafeMsgScheduled) {
+            var idx = UnsafeMsgScheduled.BinarySearch(msg);
+            if(idx < 0) {
+                idx = ~idx;
+            }
+            UnsafeMsgScheduled.Insert(idx, msg);
+            if(idx == 0) {
+                UpdateTimer(msg.Timestamp - CalcTimestamp());
+            }
+        }
+        return true;
+    }
+    private static void NotifyScheduled() {
+        lock(UnsafeMsgScheduled) {
+            long currentTimestamp = CalcTimestamp();
+            while(UnsafeMsgScheduled.Any() && UnsafeMsgScheduled[0].Timestamp < currentTimestamp) {
+                var msg = UnsafeMsgScheduled[0];
+                UnsafeMsgScheduled.RemoveAt(0);
+                msg.Actor.PushNow(msg);
+            }
+            if(UnsafeMsgScheduled.Any()) {
+                var timeout = UnsafeMsgScheduled[0].Timestamp - currentTimestamp;
+                UpdateTimer(timeout);
+            }
+        }
+    }
     private readonly Channel<Event> Queue;
-    private readonly List<Event> UnsafeMsgScheduled = new();
     private State State = null!;
-    private Task Task = Task.FromException(Exception);
+    private Task Task = null!;
     public Actor() {
-        StartTime = DateTime.Now;
-        Timer = new(o => NotifyTimer());
         Queue = Channel.CreateUnbounded<Event>();
     }
     public Task Reset(State state) {
@@ -35,7 +75,7 @@ public class Actor {
     public bool Send(object data, int delay = 0) {
         long timestamp = CalcTimestamp();
         if(delay > 0) {
-            return PostDelay(new Event(data, timestamp + delay));
+            return PushScheduled(new EventScheduled(this, data, timestamp + delay));
         } else {
             return PushNow(new Event(data, timestamp));
         }
@@ -43,54 +83,8 @@ public class Actor {
     public void Kill() {
         Queue.Writer.Complete();
     }
-    private long CalcTimestamp() {
-        return (long)((DateTime.Now - StartTime).TotalMilliseconds + 0.5);
-    }
     private bool PushNow(Event msg) {
         return Queue.Writer.TryWrite(msg);
-    }
-    private bool PostDelay(Event msg) {
-        long firstTimestamp = -1;
-        lock(UnsafeMsgScheduled) {
-            var idx = UnsafeMsgScheduled.BinarySearch(msg);
-            if(idx < 0) {
-                idx = ~idx;
-            }
-            UnsafeMsgScheduled.Insert(idx, msg);
-            if(idx == 0) {
-                firstTimestamp = msg.Timestamp;
-            }
-        }
-        if(firstTimestamp >= 0) {
-            UpdateTimer(firstTimestamp - CalcTimestamp());
-        }
-        return true;
-    }
-    private void UpdateTimer(long timeout) {
-        if(timeout < 0) {
-            timeout = 0;
-        }
-        Timer.Change(timeout, Timeout.Infinite);
-    }
-    private void NotifyTimer() {
-        List<Event> MsgReady = new();
-        long currentTimestamp = CalcTimestamp();
-        long nextTimestamp = -1;
-        lock(UnsafeMsgScheduled) {
-            while(UnsafeMsgScheduled.Any() && UnsafeMsgScheduled[0].Timestamp < currentTimestamp) {
-                MsgReady.Add(UnsafeMsgScheduled[0]);
-                UnsafeMsgScheduled.RemoveAt(0);
-            }
-            if(UnsafeMsgScheduled.Any()) {
-                nextTimestamp = UnsafeMsgScheduled[0].Timestamp;
-            }
-        }
-        foreach(var msg in MsgReady) {
-            PushNow(msg);
-        }
-        if(nextTimestamp >= 0) {
-            UpdateTimer(nextTimestamp - currentTimestamp);
-        }
     }
     private async Task Loop() {
         bool running = true;
--- Event.cs
+++ Event.cs
@@ -1,13 +1,13 @@
 namespace Actors;
 
-public struct Event: IComparable<Event> {
+public class Event: IComparable<Event> {
     public readonly object Data;
     public readonly long Timestamp;
     public Event(object data, long timestamp) {
         Data = data;
         Timestamp = timestamp;
     }
-    public int CompareTo(Event other) {
-        return (int)(Timestamp - other.Timestamp);
+    public int CompareTo(Event? other) {
+        return (int)(Timestamp - other!.Timestamp);
     }
 }
