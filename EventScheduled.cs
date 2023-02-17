9b9a75a2c259a4914f1165aa40302be8399478c9
Malizia R <rmalizia44@gmail.com>
2023-02-17 19:32:07 -0300

    Altera visibilidade de EventScheduled

--- EventScheduled.cs
+++ EventScheduled.cs
@@ -1,9 +1,9 @@
 namespace Actors;
 
 internal class EventScheduled: Event, Cancellable {
-    public readonly Actor Actor;
-    public EventScheduled? Next = null;
-    public EventScheduled(Actor actor, object data, long timestamp): base(data, timestamp) {
+    internal readonly Actor Actor;
+    internal EventScheduled? Next = null;
+    internal EventScheduled(Actor actor, object data, long timestamp): base(data, timestamp) {
         Actor = actor;
     }
     public bool Cancel() {
